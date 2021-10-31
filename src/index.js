d760877882477dc3118e6c6faf8610c21fad80eb
Zarin Rafah <zarinrafah@gmail.com>
2021-10-31 22:48:26 +0600

    second edition

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 const x ="Started learning React";
 const y="it is a sunny day"
 ReactDOM.render(
   <div>
   <h2 className="headStyling">To Do List</h2> 
-  <h3>Call Directory</h3>
-  <p>The Paragraphs module in Drupal provides editors with a component driven architecture for building pages. </p>
+  <div className="card">
+  <h3 className="cardTitle">Call Directory</h3>
+  <p className="cardDes">The Paragraphs module in Drupal provides editors with a component driven architecture for building pages. </p>
   <p>{x}</p>
   <p>29/10/2021</p>
   <p>{new Date().getFullYear()}</p>
-  <p>{"Today's weather:"+ y}</p>
+  <p className="cardFooter">{"Today's weather:"+ y}</p>
+  </div>
   </div> ,
   document.getElementById('root') //kothay dekhate chachchi
 );
